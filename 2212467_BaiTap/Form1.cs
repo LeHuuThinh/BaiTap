959e5beb4d859eb872ab1a7428d1dc3d0d8aa093
LeHuuThinh <2212467@dlu.edu.vn>
2024-08-18 22:11:11 +0700

    Add files via upload

---
+++ 2212467_BaiTap/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _2212467_BaiTap
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void vScrollBar1_Scroll(object sender, ScrollEventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(textBox1.Text + "\n" 
+                          + textBox2.Text + "\n"
+                          + textBox3.Text + "\n"
+                          + textBox4.Text + "\n"
+                          + textBox5.Text + "\n"
+                          );
+            
+        }
+
+        private void textBox6_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(textBox6.Text);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(textBox7.Text);
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
